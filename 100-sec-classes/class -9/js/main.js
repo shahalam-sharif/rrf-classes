d335d01ba2b0d3a44b504d5f030d04381bbb791a
shahalam-sharif <shahalamsharif2015@gmail.com>
2017-06-01 17:11:30 +0600

    class-009

---
+++ 100-sec-classes/class -9/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 3,
+            margin: 30,
+            loop: true,
+            nav: false,
+        });
+
+
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
