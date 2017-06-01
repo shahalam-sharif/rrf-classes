043bcdbd4816bead521b420ce70923327448ce3d
shahalam-sharif <shahalamsharif2015@gmail.com>
2017-06-01 17:10:41 +0600

    class-007

---
+++ 100-sec-classes/class -7/js/main.js
@@ -0,0 +1,12 @@
+    $(document).ready(function() {
+     
+      $(".staff-list").owlCarousel({
+          items: 4,
+          autoPlay: false,
+          margin: 30,
+          loop: true,
+          nav: true,
+          navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
+      });
+        
+    });
