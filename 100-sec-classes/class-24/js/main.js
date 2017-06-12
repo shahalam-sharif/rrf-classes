9e2ffbb315c15b16a069433e9e6965091e06e290
shahalam-sharif <shahalamsharif2015@gmail.com>
2017-06-12 14:07:46 +0600

    class 24

---
+++ 100-sec-classes/class-24/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            loop: true,
+            dots: false,
+            nav: false,
+            autoplay: true,
+
+        });
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
