5e29da99236811e187b0bebe0b42bd1c47169c95
shahalam-sharif <shahalamsharif2015@gmail.com>
2017-06-15 14:21:10 +0600

    psd to html 3

---
+++ psd-to-html-classes/class-3/js/main.js
@@ -0,0 +1,32 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+        $(".case-studies-carousel").owlCarousel({
+            items: 3,
+            margin: 30,
+            loop: true,
+            dots: false,
+            nav: false,
+
+        });
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            nav: false,
+
+        });
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
