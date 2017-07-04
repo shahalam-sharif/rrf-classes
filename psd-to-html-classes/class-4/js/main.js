171f2754f0c86c6779d4dc0d122f2a0e87152acc
shahalam-sharif <shahalamsharif2015@gmail.com>
2017-07-04 22:09:35 +0600

    Add files via upload

---
+++ psd-to-html-classes/class-4/js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+        $(".logo-carousel").owlCarousel({
+            items: 6,
+            margin: 30,
+            loop: true,
+            dots: false,
+            nav: false,
+
+        });
+        $(".case-studies-carousel").owlCarousel({
+            items: 3,
+            margin: 30,
+            loop: true,
+            dots: false,
+            nav: false,
+
+        });
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            nav: false,
+
+        });
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
